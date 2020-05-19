5317db78fbae5138452001e0f151653a4c742db2
Patrick Roach <patrickevanroach@gmail.com>
2020-05-19 14:44:45 -0600

    mvp met

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,29 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+            
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
+    low = 0
+    high = len(arr) -1
+    mid = 0
     # Your code here
 
+    while low <= high:
+        mid = (high + low) // 2
+
+        if arr[mid] < target:
+            low = mid + 1
+        elif arr[mid] > target:
+            high = mid -1
+        else:
+         return mid
+    
 
     return -1  # not found
